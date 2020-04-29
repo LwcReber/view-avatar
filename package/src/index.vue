b9f73d067749542c69c11f969465f2288fc539c1
LwcReber <1079670310@qq.com>
2020-04-29 23:54:31 +0800

    feature： 添加生成规则

--- package/src/index.vue
+++ package/src/index.vue
@@ -1,15 +1,60 @@
 <template>
   <div>
-    <img src="" alt="图来来">
+    <div>{{avatarText}}</div>
+    <img :src="src" class="avatar">
   </div>
 </template>
 
 <script>
+  import MD5 from 'js-md5'
   export default {
-    name: 'cus-avatar'
+    name: 'cus-avatar',
+    props: {
+      src: {
+        type: String,
+        default: ''
+      },
+      text: {
+        type: String,
+        default: 'fsadfds'
+      },
+      mapColors: { // 
+        type: Array,
+        default: () => []
+      }
+    },
+    watch: {
+      text (newVal) {
+        this.create(newVal)
+      }
+    },
+    mounted () {
+      this.create(this.text)
+    },
+    data () {
+      return {
+        avatarText: '',
+        color: '',
+        bgColor: ''
+      }
+    },
+    methods: {
+      create (str) {
+        let reg = /^[A-Az-z]+$/
+        str = str.trim()
+        this.avatarText = reg.test(str) ? str.slice(0, 2) : str.slice(str.length - 2, str.length)
+        let md5str = MD5(str)
+        console.log(this.avatarTextstr)
+        let firstEle = isNaN(parseInt(md5str.slice(0, 1))) ? md5str.slice(0, 1).toLowerCase().charCodeAt() - 97 : md5str.slice(0, 1)
+        let mapColor = ((this.mapColors.length && this.mapColors) || ['#567890', '#000000', '#222222', '#777777', '#666666'])
+        let bgColors = {}
+        this.color = `${mapColor[firstEle % 5]}`
+        this.bgColor = bgColors[this.color]
+      }
+    }
   }
 </script>
 
-<style lang="less" scoped>
+<style scoped>
 
 </style>
